1c8cd6a522fbae6c9368491ecf6759df3b248f94
209 <209@academicos.cesde.edu.co>
2022-09-30 20:41:10 -0500

    ejercicio 4 y 5

---
+++ ejercicio.py
@@ -0,0 +1,14 @@
+#funcion lambda que sume dos numeros
+
+#declarar la función lambda λ
+sumar = lambda num1,num2:num1+num2
+multiplicar100 =lambda numero: numero*100
+#invocar la función λ
+sumarNumeros=sumar(5,5)
+print(sumarNumeros)
+
+multiplo=multiplicar100(10)
+print(multiplo)
+
+
+
